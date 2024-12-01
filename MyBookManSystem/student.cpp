631e8947a7953e019823e356be50c90b183c74e4
ouyang <3215399126@qq.com>
2024-12-01 19:17:08 +0800

    1

--- MyBookManSystem/student.cpp
+++ MyBookManSystem/student.cpp
@@ -9,12 +9,12 @@ void Student::borrowBook(vector<Book>& vec) {
 	showBook(vec);
 	Book bo;
 	bo.deleteBook(vec);
-	cout << "书籍借阅成功";
+	cout << "书籍借阅成功" << endl;
 }
 
 // 归还图书
 void Student::returnBook(vector<Book>& vec) {
-	cout << "请输入归还书籍的相关信息";
+	cout << "请输入归还书籍的相关信息" << endl;
 	Book bo;
 	bo.appendBook(vec);
 }
